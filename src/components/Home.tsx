5fe27283f8c8b202cf27b1284313930ee98ef240
kaushikgarg0209 <kaushikgarg0209@gmail.com>
2024-05-08 02:04:57 +0530

    Home bug

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import Image
  from 'next/image';
 export default function Home() {
   return (
-    <section className="hero-section flex items-center justify-between h-screen text-black">
+    <div className="flex items-center justify-between h-screen text-black">
     <div className="text-container max-w-2xl mx-auto px-4">
       <Typography className="text-4xl md:text-6xl font-bold mb-5">Welcome to Blog Nest</Typography>
       <Typography className="text-lg md:text-xl mb-8">This is where you'll find amazing content and Dive in and explore!</Typography>
@@ -15,6 +15,6 @@ export default function Home() {
     <div className="image-container hidden md:block w-1/2">
       <Image src={HomeImg} alt="Image description" className="w-full h-full object-cover" />
     </div>
-  </section>
+  </div>
   );
 }
